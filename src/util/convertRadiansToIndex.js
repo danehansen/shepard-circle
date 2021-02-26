da094b07196191c73f1228d1c6f5b440a1d6b84d
Dane Hansen <dane@danehansen.com>
2021-02-26 15:59:54 -0800

    fix odd semitones bug

--- src/util/convertRadiansToIndex.js
+++ src/util/convertRadiansToIndex.js
@@ -5,21 +5,8 @@ import {toRadianDirection} from './math';
 export default function convertRadiansToIndex(rad, semitones, rootPitch, layoutIncrement) {
   const ROUND = 0.001;
   const CIRC = Math.PI * 2;
-
-  // const radB = round(rad, CIRC / semitones);
-  // const radC = round(modulo(radB, CIRC), ROUND);
-  // for(let i = 0; i < semitones; i++) {
-  //   const r = round(convertIndexToRadians(i, semitones, rootPitch, layoutIncrement), ROUND);
-  //   if (r === radC) {
-  //     return i;
-  //   }
-  // }
-
   const RADIANS_IN_SLICE = CIRC / semitones;
   const DEGREES_IN_SLICE = 360 / semitones;
-
-  let closestPitch = null;
-  let closestDifference = Number.MAX_VALUE;
 
   for(let i = 0; i < semitones; i++) {
     const degrees = modulo(DEGREES_IN_SLICE * (i - rootPitch) * layoutIncrement, 360);
@@ -27,21 +14,19 @@ export default function convertRadiansToIndex(rad, semitones, rootPitch, layoutI
     const lowerLimit  = radians - RADIANS_IN_SLICE / 2;
     const upperLimit = radians + RADIANS_IN_SLICE / 2;
     let adjustedRad = rad;
-    if (adjustedRad < RADIANS_IN_SLICE / 2 && upperLimit > CIRC) {
-      adjustedRad += CIRC;
-    }
-    // console.log(i, 'convertRadiansToIndex', round(degrees, ROUND), round(radians, ROUND));
-    // console.log(round(lowerLimit, ROUND), round(rad, ROUND), round(upperLimit, ROUND));
+
     if (lowerLimit <= adjustedRad && upperLimit >= adjustedRad) {
       return i;
     }
 
-    // const difference = Math.abs(radians - rad);
-    // if (difference < closestDifference) {
-    //   closestDifference = difference;
-    //   closestPitch = i;
-    // }
-  }
+    if (lowerLimit < 0 && upperLimit > 0) {
+      adjustedRad -= CIRC;
+    } else if (lowerLimit < CIRC && upperLimit > CIRC) {
+      adjustedRad += CIRC;
+    }
 
-  // return (closestPitch * layoutIncrement * rootPitch) % semitones;
+    if (lowerLimit <= adjustedRad && upperLimit >= adjustedRad) {
+      return i;
+    }
+  }
 }
