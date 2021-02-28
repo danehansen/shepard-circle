f6cdc2db616f565208757dd65d1dc420400b0032
Dane Hansen <dane@danehansen.com>
2021-02-27 20:46:36 -0800

    revamp note naming

--- src/util/findPitchNames.js
+++ src/util/findPitchNames.js
@@ -1,24 +1,14 @@
-export default function(semitones) {
-  switch(semitones) {
-    case 1:
-      return ['C'];
-    case 2:
-      return ['C', 'F#/Gb'];
-    case 3:
-      return ['C', 'E', 'G#/Ab'];
-    case 4:
-      return ['C', 'D#/Eb', 'F#/Gb', 'A'];
-    case 6:
-      return ['A#/Bb', 'C', 'D', 'E', 'F#/Gb','G#/Ab'];
-    case 12:
-      return ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
-    default:
-      if (semitones <= 26) {
-        return [...Array(semitones).keys()].map(function(key){return String.fromCharCode(97 + parseInt(key)).toUpperCase()});
+const NAMES = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
 
-      }
-      const keys = [...Array(semitones + 1).keys()];
-      keys.shift();
-      return keys;
-  }
+export default function(semitones) {
+  return [...Array(semitones).keys()].map(function(i) {
+    const nameIndexFloat = i / semitones * NAMES.length;
+    const nameIndex = Math.floor(nameIndexFloat);
+    const prefix = NAMES[nameIndex];
+    const remainder = nameIndexFloat % 1;
+    if (!remainder) {
+      return prefix;
+    }
+    return `${prefix}${String(remainder).slice(1,5)}`;
+  })
 }
